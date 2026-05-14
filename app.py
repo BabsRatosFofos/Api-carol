cfb873b4e7b3c5059a16787e55c3b6835952e3ec
aluno.etec <aluno.etec@etec.com>
2026-05-14 14:18:34 -0300

    api

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 import requests
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 CORS(app)
 
@@ -19,5 +19,5 @@ def listar_usuarios():
 def static_files(filename):
     return send_from_directory(app.root_path, filename)
 
-if _name_ == '_main_':
+if __name__ == '_main_':
     app.run(debug=True)
